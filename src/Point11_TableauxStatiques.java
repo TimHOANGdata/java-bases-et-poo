f149ded1972b76762e965a0e06b73f87a7438e78
Tim HOANG <ht.nghiem0306@gmail.com>
2022-11-16 12:29:23 +0100

    add var portee tableauStatique et TD

---
+++ src/Point11_TableauxStatiques.java
@@ -0,0 +1,31 @@
+public class Point11_TableauxStatiques {
+    public static void main(String[] args) {
+//        int a = null;
+//        double b=null; // on peut pas definir un variable null
+        double[] test =null; // on peut creer un tableau null
+
+        double notesCoursPhysique[] = new double[5];
+
+        //permettre a l'utilisateur d'entrer les notes du tableaux a partir du clavier
+        //les valeur ne doivent etre entre 0 et 20
+        notesCoursPhysique[0] = 12.5;
+        notesCoursPhysique[1] = 15;
+        notesCoursPhysique[2] = 17;
+        notesCoursPhysique[3] = 8;
+        notesCoursPhysique[4] = 11;
+        for(int i = 0; i<=4; i++){
+            System.out.println(notesCoursPhysique[i]);
+        }
+
+        // calcul de la somme des notes
+        double somme = 0;
+        for(int i=0;i<notesCoursPhysique.length;i++){
+            somme = somme + notesCoursPhysique[i];
+        }
+        System.out.println("somme des notes = " + somme);
+
+        // calcule du moyen des notes
+        double moyen = somme/notesCoursPhysique.length;
+        System.out.println("moyen des notes = " + moyen);
+    }
+}
--- src/TP13.java
+++ src/TP13.java
@@ -20,7 +20,9 @@ public class TP13 {
         for(int i = 1; i<=n;i++){
             if(i%3==0){
                 System.out.println("fizz");
-            }else if(i%5==0){
+            }else if(i%5==0) {
+                System.out.println("buzz");
+            }else if(i%3==0 || i%5==0){
                 System.out.println("fizzbuzz");
             }else{
                 System.out.printf("%d\n",i);
